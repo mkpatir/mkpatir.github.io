106ee454d3b909f37fe2dd975eb9baac2448387c
Muhammed Kemal Patır <1306150056@ogr.iu.edu.tr>
2019-04-07 15:53:39 +0300

    added mousemove

--- odev2/js/clickJS.js
+++ odev2/js/clickJS.js
@@ -3,42 +3,42 @@ $(document).ready(function(){
     var divElementDark;
     var divElementWhite;
     var isDarkMode = true;
+    var image = $('#image');
     $("#counter").html("Clicked Div = " + divCount);
     createDivElement();
-
     function getRandomColor() {
         var letters = '0123456789ABCDEF';
         var color = '#';
         for (var i = 0; i < 6; i++) {
           color += letters[Math.floor(Math.random() * 16)];
         }
-        return "background-color: " + color + ";";
-    }
+        return color;
+    };
 
     function getRandomPosition(){
         var top = Math.floor(Math.random() * ($(window).height() - 150));
         var left = Math.floor(Math.random() * ($(window).width() - 150));
         return "top: " + top + "px; left: " + left + "px;";
-    }
+    };
 
     function getRandomSize(){
         var height = Math.floor((Math.random() * 100) + 100);
         var width = Math.floor((Math.random() * 100) + 100);
         return "width: " + width + "px; height: " + height + "px; position: absolute;";
-    }
+    };
 
     function createDivElement(){
         setTimeout(function(){
             if(isDarkMode){
-                divElementDark = '<div style="' + getRandomSize() + getRandomPosition() +  getRandomColor() + ' border: 2px solid white; border-radius: 45px"></div>';
+                divElementDark = '<div style="' + getRandomSize() + getRandomPosition() +  "background-color: " + getRandomColor() + '; border: 2px solid white; border-radius: 45px"></div>';
                 $("body").append(divElementDark);
             }
             else{
-                divElementWhite = '<div style="' + getRandomSize() + getRandomPosition() +  getRandomColor() + ' border: 2px solid black; border-radius: 45px"></div>';
+                divElementWhite = '<div style="' + getRandomSize() + getRandomPosition() + "background-color: " + getRandomColor() + '; border: 2px solid black; border-radius: 45px"></div>';
                 $("body").append(divElementWhite);
             }
         },1500)
-    }
+    };
 
     $("body").on("click","div",function(){
         $(this).fadeOut(750,function(){
@@ -47,7 +47,7 @@ $(document).ready(function(){
         divCount++;
         $("#counter").html("Clicked Div = " + divCount);
         createDivElement();
-    })
+    });
 
     $("body").on("click","button",function(){
         if(isDarkMode){
@@ -64,5 +64,9 @@ $(document).ready(function(){
             $("button").css({"background-color":"#e7e7e7","color":"black","border":"none","padding":"15px 32px","font-size":"16px"});
             isDarkMode = true;
         }
-    })
+    });
+
+    document.addEventListener("mousemove",function(event){
+        image.css({"left":event.pageX + 15,"top":event.pageY + 15,"background-color":getRandomColor()});
+    });
 })
